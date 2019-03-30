b7d71f8c5992d3f94fe063e3e9684730bf984932
mfslog <jerry.chau@qq.com>
2019-03-30 16:32:25 +0800

    fix: 修复逻辑bug

--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
 module github.com/dy-platform/user-srv-session
 
 require (
-	github.com/Sirupsen/logrus v1.0.6
-	github.com/dy-gopkg/kit v0.0.0-20190310102400-3a392b27176a
+	github.com/dy-gopkg/kit v0.0.0-20190330080747-7fd93d43a1f1
 	github.com/golang/protobuf v1.3.0
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/micro/go-config v1.0.0
--- handler/handler.go
+++ handler/handler.go
@@ -2,11 +2,11 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/dy-gopkg/kit/dao/redis"
 	"github.com/dy-platform/user-srv-session/idl"
 	pb "github.com/dy-platform/user-srv-session/idl/platform/user/srv-session"
-	rgo "github.com/gomodule/redigo/redis"
 	"github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 )
@@ -14,28 +14,39 @@ import (
 type Handler struct {
 }
 
+type User struct {
+	Uid int64 `json:"uid"`
+}
+
 //Record 登记用户session
 func (h *Handler) Record(ctx context.Context, req *pb.RecordReq, resp *pb.RecordResp) error {
 	uuid := uuid.NewV4()
 	resp.BaseResp = new(base.Resp)
-	_, err := redis.Do("set", uuid, req.UID)
+	user := User{
+		Uid: req.UID,
+	}
+	value, _ := json.Marshal(user)
+	_, err := redis.Do("SET", uuid.String(), string(value))
 	if err != nil {
 		resp.BaseResp.Code = int32(base.CODE_DATA_EXCEPTION)
 		resp.BaseResp.Msg = err.Error()
-		logrus.Errorf("redis error %v", err)
+		logrus.Errorf("redis set error %v", err)
 		return nil
 	}
 	if req.ExpireTime != -1 {
-		_, err = redis.Do("expire", uuid, req.ExpireTime)
-		resp.BaseResp.Code = int32(base.CODE_DATA_EXCEPTION)
-		resp.BaseResp.Msg = err.Error()
-
-		logrus.Errorf("redis error %v", err)
-		return nil
+		_, err = redis.Do("expire", uuid.String(), req.ExpireTime)
+		if err != nil {
+			resp.BaseResp.Code = int32(base.CODE_DATA_EXCEPTION)
+			resp.BaseResp.Msg = err.Error()
+			logrus.Errorf("redis expire error %v", err)
+			return nil
+		}
 	}
-
+	resp.Token = uuid.String()
 	resp.BaseResp.Code = int32(base.CODE_OK)
 	resp.BaseResp.Msg = "success"
+
+	logrus.Debug("resp:", resp)
 	return nil
 }
 
@@ -50,13 +61,18 @@ func (h *Handler) Refresh(ctx context.Context, req *pb.RefreshReq, resp *pb.Refr
 		logrus.Errorf("redis error %v", err)
 		return nil
 	}
+
+	resp.BaseResp.Code = int32(base.CODE_OK)
+	resp.BaseResp.Msg = "success"
+
 	return nil
 }
 
 //Query 通过token 获得用户ID
 func (h *Handler) Query(ctx context.Context, req *pb.QueryReq, resp *pb.QueryResp) error {
 	value, err := redis.Do("get", req.Token)
-	uid, err := rgo.Int64(value, err)
+	user := &User{}
+	json.Unmarshal(value.([]byte), user)
 	resp.BaseResp = new(base.Resp)
 	if err != nil {
 		resp.BaseResp.Code = int32(base.CODE_DATA_EXCEPTION)
@@ -64,7 +80,7 @@ func (h *Handler) Query(ctx context.Context, req *pb.QueryReq, resp *pb.QueryRes
 		logrus.Errorf("redis error %v", err)
 		return nil
 	}
-	resp.UID = uid
+	resp.UID = user.Uid
 	resp.BaseResp.Msg = "success"
 	resp.BaseResp.Code = int32(base.CODE_OK)
 	return nil
